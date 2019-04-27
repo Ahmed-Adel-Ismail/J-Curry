381a5b63fc2406a961de60c1342790cf1ca13aa5
Ahmed <ahm3d.ad3l@gmail.com>
2019-04-27 03:54:01 +0200

    supporting java 8 with default Reactive interfaces

--- reactivesample/src/main/java/com/reactive/sample/SampleActivity.java
+++ reactivesample/src/main/java/com/reactive/sample/SampleActivity.java
@@ -21,7 +21,7 @@ public class SampleActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        new MainTest().run();
     }
 
 
